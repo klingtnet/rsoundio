ad6da47f6eb995fb8eb48ddc364e3827efb87d54
Andreas Linz <klingt.net@gmail.com>
2016-02-17 17:41:25 +0100

    Remove `println!` from stream test

--- tests/stream.rs
+++ tests/stream.rs
@@ -6,20 +6,16 @@ use std::time::Duration;
 
 #[test]
 fn test_outstream() {
-    println!("test: OutStream");
     let sio = rsoundio::SoundIo::new();
     sio.connect_backend(rsoundio::ffi::SioBackend::Dummy).unwrap();
-    println!("current backend: {}", sio.current_backend().unwrap());
     sio.flush_events();
     let fmt;
     let dev_idx = sio.default_output_device_index().unwrap();
     let mut frames: Vec<Vec<f32>> = vec![vec![], vec![]];
     let dev = sio.get_output_device(dev_idx).unwrap();
-    println!("device: {}, ref_count: {}", dev, dev.ref_count());
     let mut stream = dev.create_outstream().unwrap();
     stream.open().unwrap();
     fmt = stream.current_format().unwrap();
-    println!("current format: {}", fmt);
     let f = 4400f32;
     let sr = stream.get_sample_rate();
     let layout = stream.get_layout();
