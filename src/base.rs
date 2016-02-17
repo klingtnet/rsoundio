1f9cf21037544e5d5f6540fe1a6d563fedcbe28a
Andreas Linz <klingt.net@gmail.com>
2016-02-17 14:24:40 +0100

    Add rustdoc

--- src/base.rs
+++ src/base.rs
@@ -5,8 +5,11 @@ use std::ffi::{CString, NulError};
 use ffi;
 use stream::OutStream;
 
+/// Result wrapper that always contains a `ffi::SioError` in error case.
 pub type SioResult<T> = Result<T, ffi::SioError>;
 
+/// The base struct which can connect to various audio backends
+/// and provides methods to get in-/output `Device`s.
 pub struct SoundIo {
     context: *mut ffi::SoundIo,
 }
@@ -15,6 +18,7 @@ impl SoundIo {
         SoundIo { context: unsafe { ffi::soundio_create() } }
     }
 
+    /// Returns the number builtin channel layouts.
     pub fn channel_layout_builtin_count() -> i32 {
         let cnt = unsafe { ffi::soundio_channel_layout_builtin_count() };
         if cnt < 0 {
@@ -24,6 +28,18 @@ impl SoundIo {
         }
     }
 
+    // NOTE: Links to other types in rustdoc are not implemented
+    // yet,
+    // [see](https://internals.rust-lang.org/t/rustdoc-link-to-other-types-from-doc-comments/968).
+
+    /// Tries to connect on all available backends in order.
+    ///
+    /// Possible errors:
+    ///
+    /// - `ffi::SioError::Invalid`
+    /// - `ffi::SioError::NoMem`
+    /// - `ffi::SioError::SystemResources`
+    /// - `ffi::SioError::NoSuchClient`
     pub fn connect(&self) -> Option<ffi::SioError> {
         match unsafe { ffi::soundio_connect(self.context) } {
             ffi::SioError::None => None,
@@ -31,6 +47,16 @@ impl SoundIo {
         }
     }
 
+    /// Instead of calling ::soundio_connect you may call this function to try a
+    /// specific backend.
+    /// Possible errors:
+    ///
+    /// - `ffi::SioError::Invalid`
+    /// - `ffi::SioError::BackendUnavailable`
+    /// - `ffi::SioError::SystemResources`
+    /// - `ffi::SioError::NoSuchClient`
+    /// - `ffi::SioError::InitAudioBackend`
+    /// - `ffi::SioError::BackendDisconnected`
     pub fn connect_backend(&self, backend: ffi::SioBackend) -> Option<ffi::SioError> {
         match unsafe { ffi::soundio_connect_backend(self.context, backend) } {
             ffi::SioError::None => None,
@@ -41,6 +67,7 @@ impl SoundIo {
         }
     }
 
+    /// Returns the number of available backens.
     pub fn backend_count(&self) -> i32 {
         let cnt = unsafe { ffi::soundio_backend_count(self.context) } as i32;
         if cnt < 0 {
@@ -50,6 +77,9 @@ impl SoundIo {
         }
     }
 
+    /// Returns a backend at the specified index.
+    /// If the index is not in range [0, backend_count), then
+    /// `None` is returned.
     pub fn get_backend(&self, idx: i32) -> Option<ffi::SioBackend> {
         match unsafe { ffi::soundio_get_backend(self.context, idx) } {
             ffi::SioBackend::None => None,
@@ -57,6 +87,8 @@ impl SoundIo {
         }
     }
 
+    /// Returns the current backend or `None` if neither
+    /// `connect` nor `connect_backend` or `disconnect` was called.
     pub fn current_backend(&self) -> Option<ffi::SioBackend> {
         match unsafe { (*self.context).current_backend } {
             ffi::SioBackend::None => None,
@@ -64,30 +96,72 @@ impl SoundIo {
         }
     }
 
+    /// Returns `true` if libsoundio was compiled against `backend`.
+    /// Otherwise `false` is returned.
     pub fn have_backend(&self, backend: ffi::SioBackend) -> bool {
         unsafe { ffi::soundio_have_backend(backend) == 1u8 }
     }
 
+    /// Atomically update information for all connected devices.
+    /// It is performant to call this function many times per second.
+    ///
+    /// When you call this, the following callbacks might be called:
+    ///
+    /// - `on_device_change`
+    /// - `on_backend_disconnect`
+    ///
+    /// TODO: Implement callback registrations
+    ///
+    /// Note that if you do not care about learning about updated devices,
+    /// you might call this function only once ever and never call `wait_events`.
     pub fn flush_events(&self) {
         unsafe { ffi::soundio_flush_events(self.context) }
     }
 
+    /// This function calls `flush_events` then blocks until another event
+    /// is ready or you call `wakeup`.
+    /// Be ready for spurious wakeups.
     pub fn wait_events(&self) {
         unsafe { ffi::soundio_wait_events(self.context) }
     }
 
+    /// Makes `wait_events` stop blocking.
     pub fn wakeup(&self) {
         unsafe { ffi::soundio_wakeup(self.context) }
     }
 
+    /// If necessary you can manually trigger a device rescan. Normally you will
+    /// not ever have to call this function, as libsoundio listens to system events
+    /// for device changes and responds to them by rescanning devices and preparing
+    /// the new device information for you to be atomically replaced when you call
+    /// `flush_events`. However you might run into cases where you want to
+    /// force trigger a device rescan, for example if an ALSA device has a
+    /// `Device::probe_error.`
+    ///
+    /// After you call this you still have to use `soundio_flush_events` or
+    /// `soundio_wait_events` and then wait for the
+    /// `on_devices_change` callback.
+    ///
+    /// This can be called from any thread context except for
+    /// `OutStream::write_callback` and `InStream::read_callback`.
     pub fn force_device_scan(&self) {
         unsafe { ffi::soundio_force_device_scan(self.context) }
     }
 
+    /// Disconnects the audio backend.
     pub fn disconnect(&self) {
         unsafe { ffi::soundio_disconnect(self.context) }
     }
 
+    /// When you call `flush_events` a snapshot of all device state is
+    /// saved and these functions merely access the snapshot data. When you want
+    /// to check for new devices, call `flush_events`. Or you can call
+    /// `wait_events` to block until devices change. If an error occurs
+    /// scanning devices in a background thread, `backend_disconnect` is called
+    /// with the error code.
+
+    /// Get the number of input devices.
+    /// Returns -1 if you never called `flush_events`.
     pub fn input_device_count(&self) -> Option<i32> {
         let cnt = unsafe { ffi::soundio_input_device_count(self.context) };
         if cnt < 0 {
@@ -97,6 +171,8 @@ impl SoundIo {
         }
     }
 
+    /// Get the number of output devices.
+    /// Returns -1 if you never called `flush_events`.
     pub fn output_device_count(&self) -> Option<i32> {
         let cnt = unsafe { ffi::soundio_output_device_count(self.context) };
         if cnt < 0 {
@@ -106,6 +182,10 @@ impl SoundIo {
         }
     }
 
+    /// Always returns a device.
+    /// `idx` must be in [0, `input_device_count`)
+    /// Returns `None` if you never called `flush_events` or if you provide
+    /// invalid parameter values.
     pub fn get_input_device(&self, idx: i32) -> Option<Device> {
         let dev_ptr = unsafe { ffi::soundio_get_input_device(self.context, idx) };
         if dev_ptr.is_null() {
@@ -115,6 +195,10 @@ impl SoundIo {
         }
     }
 
+    /// Always returns a device.
+    /// `idx` must be in [0, `output_device_count`)
+    /// Returns `None` if you never called `flush_events` or if you provide
+    /// invalid parameter values.
     pub fn get_output_device(&self, idx: i32) -> Option<Device> {
         let dev_ptr = unsafe { ffi::soundio_get_output_device(self.context, idx) };
         if dev_ptr.is_null() {
@@ -124,6 +208,9 @@ impl SoundIo {
         }
     }
 
+    /// Returns the index of the default input device or `-1`
+    /// if there are no devices or if you never called
+    /// `flush_events`.
     pub fn default_input_device_index(&self) -> Option<i32> {
         match unsafe { ffi::soundio_default_input_device_index(self.context) } {
             -1 => None,
@@ -131,6 +218,9 @@ impl SoundIo {
         }
     }
 
+    /// Returns the index of the default output device or `-1`
+    /// if there are no devices or if you never called
+    /// `flush_events`.
     pub fn default_output_device_index(&self) -> Option<i32> {
         match unsafe { ffi::soundio_default_output_device_index(self.context) } {
             -1 => None,
@@ -170,6 +260,8 @@ impl Drop for SoundIo {
     }
 }
 
+/// Provides methods on channel layouts. Layout variants are defined
+/// in `ffi::enums::SioChannelLayoutId`.
 #[derive(Debug)]
 pub struct ChannelLayout {
     layout: *const ffi::SoundIoChannelLayout,
@@ -179,6 +271,8 @@ impl ChannelLayout {
         ChannelLayout { layout: raw_layout }
     }
 
+    /// Returns a builtin channel layout or `None` if
+    /// `idx` *not* in [0, `SoundIo::channel_layout_builtin_count`).
     pub fn get_builtin(idx: i32) -> Option<Self> {
         if 0 <= idx && idx < SoundIo::channel_layout_builtin_count() {
             Some(ChannelLayout::new(unsafe {
@@ -189,6 +283,7 @@ impl ChannelLayout {
         }
     }
 
+    /// Get the default builtin channel layout for the given number of channels.
     pub fn get_default(channel_count: i32) -> Option<Self> {
         if channel_count < 0 {
             None
@@ -199,7 +294,7 @@ impl ChannelLayout {
         }
     }
 
-
+    /// Return the index of `channel` in the layout, or `None` if not found.
     pub fn find_channel(&self, channel: ffi::SioChannelId) -> Option<i32> {
         match unsafe { ffi::soundio_channel_layout_find_channel(self.layout, channel) } {
             -1 => None,
@@ -207,6 +302,7 @@ impl ChannelLayout {
         }
     }
 
+    /// Populates the name field of layout if it matches a builtin one.
     pub fn detect_builtin(&mut self) -> bool {
         // This is a hack because of the transmute.
         unsafe {
@@ -215,6 +311,10 @@ impl ChannelLayout {
         }
     }
 
+    /// Iterates over `preferred_layouts`. Returns the first channel layout in
+    /// `preferred_layouts` which matches one of the channel layouts in
+    /// `available_layouts`.
+    /// Returns `None` if none matches.
     pub fn best_matching_channel_layout(preferred_layouts: &[ChannelLayout],
                                         available_layouts: &[ChannelLayout])
                                         -> Option<ChannelLayout> {
@@ -238,6 +338,7 @@ impl ChannelLayout {
         }
     }
 
+    /// Returns the number of channels in the layout.
     pub fn channel_count(&self) -> i32 {
         unsafe { (*self.layout).channel_count as i32 }
     }
@@ -258,6 +359,7 @@ impl Display for ChannelLayout {
     }
 }
 
+/// Provides methods on an audio device.
 #[derive(Debug)]
 pub struct Device {
     device: *mut ffi::SoundIoDevice,
@@ -268,36 +370,58 @@ impl Device {
     }
 
     // ref is a keyword
+    /// Add 1 to the reference count of `device`.
     pub fn inc_ref(&self) {
         unsafe { ffi::soundio_device_ref(self.device) }
     }
 
+    // Called automatically on `Device` drop.
+    /// Remove 1 to the reference count of `device`. Clean up if it was the last
+    /// reference.
     pub fn dec_ref(&self) {
         unsafe { ffi::soundio_device_unref(self.device) }
     }
 
+    /// Sorts channel layouts by channel count, descending.
     pub fn sort_channel_layouts(&self) {
         unsafe { ffi::soundio_device_sort_channel_layouts(self.device) }
     }
 
+    /// Convenience function.
+    /// Returns whether `format` is included in the device's
+    /// supported formats.
     pub fn supports_format(&self, format: ffi::SioFormat) -> bool {
         unsafe { ffi::soundio_device_supports_format(self.device, format) == 1u8 }
     }
 
+    /// Convenience function.
+    /// Returns whether `layout` is included in the device's
+    /// supported channel layouts.
     pub fn supports_layout(&self, layout: &ChannelLayout) -> bool {
         unsafe { ffi::soundio_device_supports_layout(self.device, layout.layout) == 1u8 }
     }
 
+    /// Convenience function.
+    /// Returns whether `sample_rate` is included in the
+    /// device's supported sample rates.
     pub fn supports_sample_rate(&self, sample_rate: i32) -> bool {
         unsafe {
             ffi::soundio_device_supports_sample_rate(self.device, sample_rate as c_int) == 1u8
         }
     }
 
+    /// Convenience function.
+    /// Returns the available sample rate nearest to
+    /// `sample_rate`, rounding up.
     pub fn nearest_sample_rate(&self, sample_rate: i32) -> i32 {
         unsafe { ffi::soundio_device_nearest_sample_rate(self.device, sample_rate) as i32 }
     }
 
+    // TODO: Return a Result and `ffi::SioError::NoMem` if stream is `None`.
+    /// Allocates memory and sets defaults. Next you should fill out the struct fields
+    /// and then call `OutStream::open`.
+    /// Sets all fields to defaults.
+    /// Returns `None` if and only if memory could not be allocated.
     pub fn create_outstream(&self) -> Option<OutStream> {
         let stream_ptr = unsafe { ffi::soundio_outstream_create(self.device) };
         if stream_ptr.is_null() {
@@ -307,8 +431,26 @@ impl Device {
         }
     }
 
+    /// Returns the number of references on this device.
     pub fn ref_count(&self) -> i32 {
         unsafe { (*self.device).ref_count as i32 }
+    }
+
+    /// This is set to a `ffi::SioError` representing the result of the device
+    /// probe. Ideally this will be `ffi::SioError::None` in which case all the
+    /// fields of the device will be populated. If there is an error code here
+    /// then information about formats, sample rates, and channel layouts might
+    /// be missing.
+    ///
+    /// Possible errors:
+    ///
+    /// - `ffi::SioError::OpeningDevice`
+    /// - `ffi::SioError::NoMem`
+    pub fn probe_error(&self) -> Option<ffi::SioError> {
+        match unsafe { (*self.device).probe_error } {
+            ffi::SioError::None => None,
+            error @ _ => Some(error),
+        }
     }
 }
 impl Display for Device {
--- src/stream.rs
+++ src/stream.rs
@@ -6,7 +6,15 @@ use base::*;
 
 macro_rules! write_stream {
     ($name:ident, $t:ty) => (
-        pub fn $name(&self,min_frame_count: i32,buffers: &Vec<Vec<$t>>) -> SioResult<i32> {
+        /// Expects a vector of `channel_count` channel `buffers`
+        /// containing audio data that are written into
+        /// the output device buffer.
+        /// Returns the number of actually written frames.
+        /// If the provided buffers contain less frames
+        /// than `min_frame_count`, or less buffers
+        /// as `channel_count` are provided,
+        /// then a `ffi::SioError::Invalid` is returned.
+        pub fn $name(&self, min_frame_count: i32, buffers: &Vec<Vec<$t>>) -> SioResult<i32> {
             let channel_count = self.get_layout().channel_count();
             // check if buffer contains frames for all channels
             if buffers.len() < channel_count as usize {
@@ -79,6 +87,7 @@ impl<'a> Drop for OutStreamCallbacks<'a> {
     fn drop(&mut self) {}
 }
 
+/// An audio output stream, returned from a `Device`.
 pub struct OutStream<'a> {
     stream: *mut ffi::SoundIoOutStream,
     callbacks: Box<OutStreamCallbacks<'a>>,
@@ -92,6 +101,30 @@ impl<'a> OutStream<'a> {
         }
     }
 
+    /// After you call this function, `OutStream::software_latency` is set to
+    /// the correct value.
+    ///
+    /// The next thing to do is call `outstream_start`.
+    /// If this function returns an error, the outstream is in an invalid state and
+    /// you must call `destroy` on it.
+    ///
+    /// Possible errors:
+    ///
+    /// - `ffi::SioErrorInvalid`
+    ///     - TODO: implement format setting
+    ///     - SoundIoDevice::aim is not #SoundIoDeviceAimOutput
+    ///     - SoundIoOutStream::format is not valid
+    ///     - SoundIoOutStream::channel_count is greater than #SOUNDIO_MAX_CHANNELS
+    /// - `ffi::SioError::NoMem`
+    /// - `ffi::SioError::OpeningDevice`
+    /// - `ffi::SioError::BackendDisconnected`
+    /// - `ffi::SioError::SystemResources`
+    /// - `ffi::SioError::NoSuchClient` - when JACK returns `JackNoSuchClient`
+    /// - `ffi::SioErrorOpeningDevice`
+    /// - `ffi::SioErrorIncompatibleBackend` - `OutStream::channel_count` is
+    ///   greater than the number of channels the backend can handle.
+    /// - `ffi::SioErrorIncompatibleDevice` - stream parameters requested are not
+    ///   compatible with the chosen device.
     pub fn open(&self) -> Option<ffi::SioError> {
         match unsafe { ffi::soundio_outstream_open(self.stream) } {
             ffi::SioError::None => None,
@@ -99,6 +132,16 @@ impl<'a> OutStream<'a> {
         }
     }
 
+    /// After you call this function, the registered `write_callback` will be called.
+    ///
+    /// This function might directly call the `write_callback`.
+    ///
+    /// Possible errors:
+    ///
+    /// - `ffi::SioError::Streaming`
+    /// - `ffi::SioError::NoMem`
+    /// - `ffi::SioError::SystemResources`
+    /// - `ffi::SioError::BackendDisconnected`
     pub fn start(&self) -> Option<ffi::SioError> {
         match unsafe { ffi::soundio_outstream_start(self.stream) } {
             ffi::SioError::None => None,
@@ -106,6 +149,24 @@ impl<'a> OutStream<'a> {
         }
     }
 
+    /// Registers the given callback as `write_callback` that is called as soon as you call `start`.
+    ///
+    /// In this callback, you call `OutStream::write_stream_FMT` where `FMT` is one of the supported
+    /// format, `u16`, `f32` etc.
+    ///
+    /// `frame_count_max` will always be greater than 0. Note that you
+    /// should write as many frames as you can; `frame_count_min` might be 0 and
+    /// you can still get a buffer underflow if you always write
+    /// `frame_count_min` frames.
+    ///
+    /// For Dummy, ALSA, and PulseAudio, `frame_count_min` will be 0. For JACK
+    /// and CoreAudio `frame_count_min` will be equal to `frame_count_max`.
+    ///
+    /// The code in the supplied function must be suitable for real-time
+    /// execution. That means that it cannot call functions that might block
+    /// for a long time. This includes all I/O functions (disk, TTY, network),
+    /// malloc, free, printf, pthread_mutex_lock, sleep, wait, poll, select,
+    /// pthread_join, pthread_cond_wait, etc.
     pub fn register_write_callback<W>(&mut self, callback: Box<W>)
         where W: Fn(OutStream, i32, i32) + 'a
     {
@@ -120,6 +181,10 @@ impl<'a> OutStream<'a> {
         }
     }
 
+    /// Registers the given callback as `underflow_callback`.
+    /// This *optional* callback happens when the sound device runs out of buffered audio data to play.
+    /// After this occurs, the outstream waits until the buffer is full to resume playback.
+    /// This is called from the `OutStream::write_callback` thread context.
     pub fn register_underflow_callback<U>(&mut self, callback: Box<U>)
         where U: Fn(OutStream) + 'a
     {
@@ -133,6 +198,12 @@ impl<'a> OutStream<'a> {
         }
     }
 
+    /// *Optional* callback. `err` is always `ffi::SioError::ErrorStreaming`.
+    /// This is an unrecoverable error. The stream is in an
+    /// invalid state and must be destroyed, call `OutStream::destroy`.
+    /// If you do not supply `error_callback`, the default callback will print
+    /// a message to stderr and then call `abort`.
+    /// This is called from the `OutStream::write_callback` thread context.
     pub fn register_error_callback<E>(&mut self, callback: Box<E>)
         where E: Fn(OutStream, ffi::SioError) + 'a
     {
@@ -177,6 +248,19 @@ impl<'a> OutStream<'a> {
         }
     }
 
+    /// Clears the output stream buffer.
+    /// This function can be called from any thread.
+    /// This function can be called regardless of whether the outstream is paused
+    /// or not.
+    /// Some backends do not support clearing the buffer. On these backends this
+    /// function will return `ffi::SioError::IncompatibleBackend`.
+    /// Some devices do not support clearing the buffer. On these devices this
+    /// function might return `ffi::SioError::IncompatibleDevice`.
+    /// Possible errors:
+    ///
+    /// - `ffi::SioError::Streaming`
+    /// - `ffi::SioError::IncompatibleBackend`
+    /// - `ffi::SioError::IncompatibleDevice`
     pub fn clear_buffer(&self) -> Option<ffi::SioError> {
         match unsafe { ffi::soundio_outstream_clear_buffer(self.stream) } {
             ffi::SioError::None => None,
@@ -184,6 +268,26 @@ impl<'a> OutStream<'a> {
         }
     }
 
+    /// If the underlying backend and device support pausing, this pauses the
+    /// stream. `OutStream::write_callback` may be called a few more times if
+    /// the buffer is not full.
+    /// Pausing might put the hardware into a low power state which is ideal if your
+    /// software is silent for some time.
+    /// This function may be called from any thread context, including
+    /// `OutStream::write_callback`.
+    /// Pausing when already paused or unpausing when already unpaused has no
+    /// effect and returns `None`.
+    ///
+    /// Possible errors:
+    ///
+    /// - `ffi::SioError::BackendDisconnected`
+    /// - `ffi::SioError::Streaming`
+    /// - `ffi::SioError::IncompatibleDevice` - device does not support
+    ///   pausing/unpausing. This error code might not be returned even if the
+    ///   device does not support pausing/unpausing.
+    /// - `ffi::SioError::IncompatibleBackend` - backend does not support
+    ///   pausing/unpausing.
+    /// - `ffi::SioError::Invalid` - outstream not opened and started
     pub fn pause(&self, pause: bool) -> Option<ffi::SioError> {
         let pause_c_bool = match pause {
             true => 1u8,
@@ -195,6 +299,16 @@ impl<'a> OutStream<'a> {
         }
     }
 
+    /// Obtain the total number of seconds that the next frame written after the
+    /// last frame written will take to become
+    /// audible.
+    /// This includes both software and hardware latency.
+    ///
+    /// This function must be called only from within `OutStream::write_callback`.
+    ///
+    /// Possible errors:
+    ///
+    /// - `ffi::SioError::Streaming`
     pub fn get_latency(&self) -> SioResult<f64> {
         let mut latency = 0.0f64;
         match unsafe {
@@ -206,6 +320,8 @@ impl<'a> OutStream<'a> {
 
     }
 
+    /// Returns the current `format` or a `ffi::SioError::Invalid` if
+    /// the format is not set.
     pub fn current_format(&self) -> SioResult<ffi::SioFormat> {
         match unsafe { (*self.stream).format } {
             ffi::SioFormat::Invalid => Err(ffi::SioError::Invalid),
@@ -213,20 +329,26 @@ impl<'a> OutStream<'a> {
         }
     }
 
+    /// Returns the channel layout of the output stream.
     pub fn get_layout(&self) -> ChannelLayout {
         ChannelLayout::new(unsafe { &(*self.stream).layout })
     }
 
+    /// Returns the sample rate of the output stream.
     pub fn get_sample_rate(&self) -> i32 {
         unsafe { (*self.stream).sample_rate as i32 }
     }
 
+    /// Returns the underlying device of the output stream.
     pub fn get_device(&self) -> Device {
         let dev = Device::new(unsafe { (*self.stream).device });
         dev.inc_ref();
         dev
     }
 
+    /// Destroys the output stream.
+    /// Calls this when your application shuts down.
+    /// NOTE: This can break if a callback is still active.
     pub fn destroy(&self) {
         unsafe { ffi::soundio_outstream_destroy(self.stream) }
     }
