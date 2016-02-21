b2780c2e7c97c11bed0ceb9d64ad0b9370fb1ba0
Andreas Linz <klingt.net@gmail.com>
2016-02-21 21:27:02 +0100

    +C

--- build.rs
+++ build.rs
@@ -83,8 +83,8 @@ fn build(target: String) {
     let build_dir = soundio_root.join("build");
     fs::create_dir(&build_dir).unwrap();
 
-    let cc = env::var("MAKE_C_COMPILER").unwrap();
-    let cxx = env::var("MAKE_CXX_COMPILER").unwrap();
+    let cc = env::var("CMAKE_C_COMPILER").unwrap();
+    let cxx = env::var("CMAKE_CXX_COMPILER").unwrap();
 
     // run cmake
     cmake::Config::new(&soundio_root)
@@ -95,8 +95,8 @@ fn build(target: String) {
         .define("BUILD_TESTS", "OFF")
         .define("BUILD_STATIC_LIBS", "ON")
         .define("ENABLE_JACK", "OFF")
-        .define("MAKE_C_COMPILER", cc)
-        .define("MAKE_CXX_COMPILER", cxx)
+        .define("CMAKE_C_COMPILER", cc)
+        .define("CMAKE_CXX_COMPILER", cxx)
         .build();
 
     // make install
